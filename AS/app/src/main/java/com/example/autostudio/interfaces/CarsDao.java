826f67a52d37d869030bd8b8990ae075e5225065
Mete <meteabduraman@gmail.com>
2021-01-10 23:17:16 +0200

    update rca itp carsDao

--- AS/app/src/main/java/com/example/autostudio/interfaces/CarsDao.java
+++ AS/app/src/main/java/com/example/autostudio/interfaces/CarsDao.java
@@ -7,6 +7,7 @@ import androidx.room.Transaction;
 
 import com.example.autostudio.classes.Car;
 
+import java.util.Date;
 import java.util.List;
 
 @Dao
@@ -15,9 +16,6 @@ public interface CarsDao {
     @Transaction
     @Insert
     long insertCar(Car car);
-
-//    @Insert
-//    void insertEvents(List<Event> events);
 
     @Query("Select * from cars")
     List<Car> getAll();
@@ -30,6 +28,12 @@ public interface CarsDao {
 
     @Query("Update cars set tankCapacity=:tankCapacity where car_id=:carId")
     void updateCarTankCapacityById(double tankCapacity, long carId);
+
+    @Query("Update cars set expDateRCA=:expDateRCA where car_id=:carId")
+    void updateCarRCAById(Date expDateRCA, long carId);
+
+    @Query("Update cars set expDateITP=:expDateITP where car_id=:carId")
+    void updateCarITPById(Date expDateITP, long carId);
 
     @Query("Delete from cars")
     void deleteAll();
